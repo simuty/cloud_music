8569712054c18b108c6079c11041028adedea90f
huanghaowei@mwee.cn <huanghaowei@mwee.cn>
2020-04-14 18:52:48 +0800

    UI finish

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {};
+}
+
+class reducer extends Component {
+    render() {
+        return <div>ssss</div>;
+    }
+}
+
+export default connect(mapStateToProps)(reducer);
